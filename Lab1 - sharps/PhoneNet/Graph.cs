1acb2bb56de86302feb7ca8a56bf2c12c96b087d
percova <viktoriyacap@gmail.com>
2018-06-12 17:32:48 +0300

    Add files via upload

---
+++ Lab1 - sharps/PhoneNet/Graph.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace PhoneNet
+{
+    public class Graph
+    {
+        private int nodeNum;
+        private double[,] adjacencyMatrix;
+
+        public Graph(int nodeNum)
+        {
+            adjacencyMatrix = new double[nodeNum, nodeNum];
+            this.nodeNum = nodeNum;
+            Random rand = new Random();
+            for (int i = 0; i < adjacencyMatrix.Length; i++)
+            {
+                for (int j = 0; j < adjacencyMatrix.Length; j++)
+                {
+                    adjacencyMatrix[i, j] = rand.Next();
+                }
+            }
+        }
+
+        public Graph(int nodeNum, double[,] adjacencyMatrix)
+        {
+            adjacencyMatrix = new double[nodeNum, nodeNum];
+            this.nodeNum = nodeNum;
+            this.adjacencyMatrix = adjacencyMatrix;
+        }
+
+        public void PrimsAlgo()
+        {
+            int[] nodeSequence = new int[nodeNum];
+            double min = 0;
+
+            for (int i = 0; i < nodeNum; i++)
+            {
+                for (int j = 0; j < nodeNum; j++)
+                {
+                    if (nodeSequence[j] != i && adjacencyMatrix[i,j] != 0 && adjacencyMatrix[i,j] < min)
+                    {
+                        min += adjacencyMatrix[i, j];
+                        nodeSequence[i] = i + 1;
+                    }
+                }
+            }
+            
+            Console.Write("1");
+            for (int i = 0; i < nodeNum; i++)
+            {
+                Console.Write("->"  + nodeSequence[i] );
+            }
+            Console.WriteLine();
+            Console.WriteLine(min);
+        } 
+    }
+}
---
+++ Lab1 - sharps/PhoneNet/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace PhoneNet
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter num of nodes: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            double[,] costs;
+            costs = new double[num, num];
+            Console.WriteLine("Enter adjacency matrix: ");
+            for (int i = 0; i < num; i++)
+            {
+                string enterString = Console.ReadLine();
+                string[] massiveString = enterString.Split(new Char[] { ' ' });
+                for (int j = 0; j < massiveString.Length; j++)
+                {
+                    costs[i, j] = int.Parse(massiveString[j]);
+                }
+
+            }
+
+            Graph wiki = new Graph(num, costs);
+            wiki.PrimsAlgo();
+        }
+    }
+}
