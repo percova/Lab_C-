d23e2b36698f67bb7d45787c6bfd42002180b8d5
percova <viktoriyacap@gmail.com>
2018-10-25 22:20:38 +0300

    Delete CarFactory.cs

--- CarFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CarManufacturingFactory
-{
-    public abstract class CarFactory
-    {
-        public abstract PassengerCar CreatePassengerCar();
-        public abstract Truck CreateTruck();
-        public abstract Bus CreateBus();
-    }
-}
